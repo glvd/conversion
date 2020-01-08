59bcb45759881a4542d1429e4f211f8f56497978
godcong <jumbycc@163.com>
2020-01-08 15:24:15 +0800

    update conversion

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,27 @@
 package main
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
 
-var rootCMD = cobra.Command{Use: "conversion"}
+const programName = `conversion`
+
+var rootCmd = &cobra.Command{
+	Use:     "conversion [command]",
+	Short:   "call conversion command",
+	Version: "v0.0.1",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("arguments [command] was not inputted")
+	},
+	DisableSuggestions:         false,
+	SuggestionsMinimumDistance: 1,
+}
 
 func main() {
-
+	rootCmd.AddCommand()
+	e := rootCmd.Execute()
+	if e != nil {
+		panic(e)
+	}
 }
