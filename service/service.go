221df542073a1387b67696ebb04a4ca7c8085b94
godcong <jumbycc@163.com>
2020-01-08 14:17:46 +0800

    update conversion

--- service/service.go
+++ service/service.go
@@ -3,27 +3,37 @@ package service
 import (
 	"github.com/RichardKnop/machinery/v1"
 	"github.com/RichardKnop/machinery/v1/config"
+	"sync"
 )
 
-var _server *machinery.Server
+// Service ...
+type Service struct {
+	*machinery.Server
+}
+
+var _service *Service
+var _once = sync.Once{}
 
 // NewService ...
-func NewService() error {
-	var cfg = &config.Config{
-		Broker:        "amqp://guest:guest@localhost:5672/",
-		DefaultQueue:  "machinery_tasks",
-		ResultBackend: "amqp://guest:guest@localhost:5672/",
-		AMQP: &config.AMQPConfig{
-			Exchange:     "machinery_exchange",
-			ExchangeType: "direct",
-			BindingKey:   "machinery_task",
-		},
-	}
+func NewService() *Service {
+	_once.Do(func() {
+		var cfg = &config.Config{
+			Broker:        "amqp://guest:guest@localhost:5672/",
+			DefaultQueue:  "machinery_tasks",
+			ResultBackend: "amqp://guest:guest@localhost:5672/",
+			AMQP: &config.AMQPConfig{
+				Exchange:     "machinery_exchange",
+				ExchangeType: "direct",
+				BindingKey:   "machinery_task",
+			},
+		}
 
-	server, err := machinery.NewServer(cfg)
-	if err != nil {
-		return err
-	}
-	_server = server
-	return nil
+		server, err := machinery.NewServer(cfg)
+		if err != nil {
+			return
+		}
+		_service = &Service{Server: server}
+	})
+
+	return _service
 }
